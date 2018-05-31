7c76d46a353f47e3a6a6d15564bed6395f433406
guyam <39163977+guyam@users.noreply.github.com>
2018-05-31 14:46:02 +0300

    Update FilterException.java

--- FilterException.java
+++ FilterException.java
@@ -4,7 +4,7 @@ public class FilterException extends CommandException {
 
     private static final long serialVersionUID = 1L;
 
-    public final String filterMsg = "ERROR: Problem with FILTER sub-section name in file \n";
+    public final String filterMsg = "ERROR: FILTER sub-section missing \n";
 
 
     @Override
